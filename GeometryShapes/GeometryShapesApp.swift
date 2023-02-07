2dd7c381bb2f016138a1c17428e87a255547b4ef
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2023-02-07 18:09:14 +0100

    Initial Commit

---
+++ GeometryShapes/GeometryShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryShapesApp.swift
+//  GeometryShapes
+//
+//  Created by Mohamed Alwakil on 2023-02-07.
+//
+
+import SwiftUI
+
+@main
+struct GeometryShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
